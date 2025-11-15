bf3b7b7acc65059c19d5889750be639a9801195a
Lee Hodgkinson <leehodg@gmail.com>
2025-11-15 12:56:28 +0400

    handling different requests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
@@ -19,6 +20,31 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!(
+            "{}Content-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
 }
